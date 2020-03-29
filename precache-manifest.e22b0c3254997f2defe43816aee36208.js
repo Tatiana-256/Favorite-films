30561c5cca08fd57b8274a6c2cf7f24f58f05d87
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-03-29 17:18:23 +0300

    Updates

---
+++ precache-manifest.e22b0c3254997f2defe43816aee36208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60224047300cf749c7d8fe3296b6eda4",
+    "url": "/Favorite-films/index.html"
+  },
+  {
+    "revision": "563ce86f801f0466d85b",
+    "url": "/Favorite-films/static/css/main.037bfb37.chunk.css"
+  },
+  {
+    "revision": "c3cd053544325532bf5f",
+    "url": "/Favorite-films/static/js/2.64b76ab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Favorite-films/static/js/2.64b76ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563ce86f801f0466d85b",
+    "url": "/Favorite-films/static/js/main.65dafbdf.chunk.js"
+  },
+  {
+    "revision": "8b4673e73ba215073dd5",
+    "url": "/Favorite-films/static/js/runtime-main.1dcf35e5.js"
+  }
+]);
